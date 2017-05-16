52dde7cdd3d5f51c1026e5e65eaf01f2caba9556
Michel Sousa <michelsousacarvalho@gmail.com>
2017-05-16 01:52:39 -0300

    Ajustes Layout, tela de mostrar noticias

---
+++ Info/Info/NSDate+FormattedDate.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+FormattedDate.h
+//  Info
+//
+//  Created by Michel de Sousa Carvalho on 16/05/17.
+//  Copyright © 2017 Michel de Sousa Carvalho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (FormattedDate)
+
+@end
